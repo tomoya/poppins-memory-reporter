6900715f301a07969dd4308ba3cdcf0e2165a253
tomoya <tomoya.ton@gmail.com>
2018-06-07 00:39:35 +0900

    Apply eslint

--- src/index.js
+++ src/index.js
@@ -1,18 +1,24 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer");
 
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('https://ps.poppins.co.jp/nursery/login/index?redirect=%2Fnursery%2Fmypage%2FpoppinsMemory');
-  await page.type('#userId', process.env.USER_ID);
-  await page.type('#password', process.env.PASSWORD);
-  await page.click('button[type=submit]');
+  await page.goto("https://ps.poppins.co.jp/nursery/login/index?redirect=%2Fnursery%2Fmypage%2FpoppinsMemory");
+  await page.type("#userId", process.env.USER_ID);
+  await page.type("#password", process.env.PASSWORD);
+  await page.click("button[type=submit]");
   await page.waitForNavigation();
-  await page.click('#previousDay');
+  await page.click("#previousDay");
   await page.waitForNavigation();
-  const milkTime1 = await page.evaluate(() => document.querySelector('#off > tbody > tr:nth-child(5) > td:nth-child(2) > textarea').value);
-  const milkTime2 = await page.evaluate(() => document.querySelector('#off > tbody > tr:nth-child(6) > td:nth-child(2) > textarea').value);
-  const summary = await page.evaluate(() => document.querySelector('#off > tbody > tr:nth-child(10) > td:nth-child(2) > textarea').value);
+  const milkTime1 = await page.evaluate(
+    () => document.querySelector("#off > tbody > tr:nth-child(5) > td:nth-child(2) > textarea").value
+  );
+  const milkTime2 = await page.evaluate(
+    () => document.querySelector("#off > tbody > tr:nth-child(6) > td:nth-child(2) > textarea").value
+  );
+  const summary = await page.evaluate(
+    () => document.querySelector("#off > tbody > tr:nth-child(10) > td:nth-child(2) > textarea").value
+  );
   console.log(milkTime1);
   console.log(milkTime2);
   console.log(summary);
