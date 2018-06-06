a09b69b1a176b656bf6298f59eb14b7267e3492c
tomoya <tomoya.ton@gmail.com>
2018-06-06 23:34:43 +0900

    Set redirect url to poppins memory and remove unnecessary goto()

--- index.js
+++ index.js
@@ -3,12 +3,11 @@ const puppeteer = require('puppeteer');
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('https://ps.poppins.co.jp/nursery/login/index');
+  await page.goto('https://ps.poppins.co.jp/nursery/login/index?redirect=%2Fnursery%2Fmypage%2FpoppinsMemory');
   await page.type('#userId', process.env.USER_ID);
   await page.type('#password', process.env.PASSWORD);
   await page.click('button[type=submit]');
   await page.waitForNavigation();
-  await page.goto('https://ps.poppins.co.jp/nursery/mypage/poppinsMemory');
   await page.click('#previousDay');
   await page.waitForNavigation();
   const milkTime1 = await page.evaluate(() => document.querySelector('#off > tbody > tr:nth-child(5) > td:nth-child(2) > textarea').value);
