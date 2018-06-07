0b339bdaa3a603be538bf988c0f4d873d1b351fd
tomoya <tomoya.ton@gmail.com>
2018-06-07 13:41:45 +0900

    Set channel if SLACK_CHANNEL is defined

--- src/utils/slack.js
+++ src/utils/slack.js
@@ -2,9 +2,9 @@ import { IncomingWebhook } from "@slack/client";
 
 const url = process.env.SLACK_WEBHOOK_URL;
 const defaults = {
-  channel: process.env.SLACK_CHANNEL,
   username: "Poppins Memory Reporter"
 };
+if (process.env.SLACK_CHANNEL) defaults.channel = process.env.SLACK_CHANNEL;
 const webhook = new IncomingWebhook(url, defaults);
 
 export default webhook;
