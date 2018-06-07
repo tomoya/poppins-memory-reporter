0f0412f7e48b8c89f9c2ad7845510a60a040f071
tomoya <tomoya.ton@gmail.com>
2018-06-07 14:59:10 +0900

    Handle errors during browser startup

--- src/index.js
+++ src/index.js
@@ -24,71 +24,78 @@ import POPPINS_BLUE_COLOR from "./constants/color";
 
 (async () => {
   const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto(LOGIN_URL);
-  await page.type(USER_ID_INPUT, process.env.USER_ID);
-  await page.type(PASSWORD_INPUT, process.env.PASSWORD);
-  await page.click(LOGIN_BUTTON);
-  await page.waitForNavigation();
-  if (!process.env.REPORT_TODAY) {
-    await page.click(PREVIOUS_DAY_BUTTON);
+  // Don't stop process if Don't close browser
+  try {
+    const page = await browser.newPage();
+    await page.goto(LOGIN_URL);
+    await page.type(USER_ID_INPUT, process.env.USER_ID);
+    await page.type(PASSWORD_INPUT, process.env.PASSWORD);
+    await page.click(LOGIN_BUTTON);
     await page.waitForNavigation();
-  }
-  const getValue = selector => document.querySelector(selector).value;
-  const getInnerText = selector => document.querySelector(selector).innerText;
-  const reportDate = await page.evaluate(getInnerText, REPORT_DATE);
-  const lunchTime = await page.evaluate(getValue, LUNCH_TIME_TEXTAREA);
-  const snackTime = await page.evaluate(getValue, SNACK_TIME_TEXTAREA);
-  const poo = await page.evaluate(getValue, POO_TEXTAREA);
-  const sleepStartHour1 = await page.evaluate(getValue, SLEEP_START_HOUR_1_INPUT);
-  const sleepStartMinute1 = await page.evaluate(getValue, SLEEP_START_MINUTE_1_INPUT);
-  const sleepEndHour1 = await page.evaluate(getValue, SLEEP_END_HOUR_1_INPUT);
-  const sleepEndMinute1 = await page.evaluate(getValue, SLEEP_END_MINUTE_1_INPUT);
-  const sleepStartHour2 = await page.evaluate(getValue, SLEEP_START_HOUR_2_INPUT);
-  const sleepStartMinute2 = await page.evaluate(getValue, SLEEP_START_MINUTE_2_INPUT);
-  const sleepEndHour2 = await page.evaluate(getValue, SLEEP_END_HOUR_2_INPUT);
-  const sleepEndMinute2 = await page.evaluate(getValue, SLEEP_END_MINUTE_2_INPUT);
-  const looing = await page.evaluate(getValue, LOOKING_TEXTAREA);
-  if (looing === "") {
-    await webhook.send(`${reportDate}のレポートはありません`);
-  } else {
-    const IncomingWebhookSendArguments = {
-      attachments: [
-        {
-          fallback: "poppins memory summary post",
-          color: POPPINS_BLUE_COLOR,
-          title: `${reportDate}のポピンズメモリー`,
-          fields: [
-            {
-              title: "昼食",
-              value: lunchTime,
-              short: true
-            },
-            {
-              title: "午後・おやつ",
-              value: snackTime,
-              short: true
-            },
-            {
-              title: "排泄",
-              value: poo,
-              short: true
-            },
-            {
-              title: "睡眠",
-              value: `${sleepStartHour1}:${sleepStartMinute1}〜${sleepEndHour1}:${sleepEndMinute1}\n${sleepStartHour2}:${sleepStartMinute2}〜${sleepEndHour2}:${sleepEndMinute2}`,
-              short: true
-            },
-            {
-              title: "今日の様子",
-              value: looing,
-              short: false
-            }
-          ]
-        }
-      ]
-    };
-    await webhook.send(IncomingWebhookSendArguments);
+    if (!process.env.REPORT_TODAY) {
+      await page.click(PREVIOUS_DAY_BUTTON);
+      await page.waitForNavigation();
+    }
+    const getValue = selector => document.querySelector(selector).value;
+    const getInnerText = selector => document.querySelector(selector).innerText;
+    const reportDate = await page.evaluate(getInnerText, REPORT_DATE);
+    const lunchTime = await page.evaluate(getValue, LUNCH_TIME_TEXTAREA);
+    const snackTime = await page.evaluate(getValue, SNACK_TIME_TEXTAREA);
+    const poo = await page.evaluate(getValue, POO_TEXTAREA);
+    const sleepStartHour1 = await page.evaluate(getValue, SLEEP_START_HOUR_1_INPUT);
+    const sleepStartMinute1 = await page.evaluate(getValue, SLEEP_START_MINUTE_1_INPUT);
+    const sleepEndHour1 = await page.evaluate(getValue, SLEEP_END_HOUR_1_INPUT);
+    const sleepEndMinute1 = await page.evaluate(getValue, SLEEP_END_MINUTE_1_INPUT);
+    const sleepStartHour2 = await page.evaluate(getValue, SLEEP_START_HOUR_2_INPUT);
+    const sleepStartMinute2 = await page.evaluate(getValue, SLEEP_START_MINUTE_2_INPUT);
+    const sleepEndHour2 = await page.evaluate(getValue, SLEEP_END_HOUR_2_INPUT);
+    const sleepEndMinute2 = await page.evaluate(getValue, SLEEP_END_MINUTE_2_INPUT);
+    const looing = await page.evaluate(getValue, LOOKING_TEXTAREA);
+    if (looing === "") {
+      await webhook.send(`${reportDate}のレポートはありません`);
+    } else {
+      const IncomingWebhookSendArguments = {
+        attachments: [
+          {
+            fallback: "poppins memory summary post",
+            color: POPPINS_BLUE_COLOR,
+            title: `${reportDate}のポピンズメモリー`,
+            fields: [
+              {
+                title: "昼食",
+                value: lunchTime,
+                short: true
+              },
+              {
+                title: "午後・おやつ",
+                value: snackTime,
+                short: true
+              },
+              {
+                title: "排泄",
+                value: poo,
+                short: true
+              },
+              {
+                title: "睡眠",
+                value: `${sleepStartHour1}:${sleepStartMinute1}〜${sleepEndHour1}:${sleepEndMinute1}\n${sleepStartHour2}:${sleepStartMinute2}〜${sleepEndHour2}:${sleepEndMinute2}`,
+                short: true
+              },
+              {
+                title: "今日の様子",
+                value: looing,
+                short: false
+              }
+            ]
+          }
+        ]
+      };
+      await webhook.send(IncomingWebhookSendArguments);
+    }
+  } catch (e) {
+    webhook.send(`次のエラーが発生しました\n${e}`);
   }
   await browser.close();
-})();
+})().catch(e => {
+  webhook.send(`次のエラーが発生しました\n${e}`);
+});
