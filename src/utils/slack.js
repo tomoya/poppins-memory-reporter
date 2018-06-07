995892e82dd2ab4eb5046271754ae3a61915f64c
tomoya <tomoya.ton@gmail.com>
2018-06-07 13:37:38 +0900

    Modify slack username

--- src/utils/slack.js
+++ src/utils/slack.js
@@ -3,7 +3,7 @@ import { IncomingWebhook } from "@slack/client";
 const url = process.env.SLACK_WEBHOOK_URL;
 const defaults = {
   channel: process.env.SLACK_CHANNEL,
-  username: "Poppins Memory Bot"
+  username: "Poppins Memory Reporter"
 };
 const webhook = new IncomingWebhook(url, defaults);
 
